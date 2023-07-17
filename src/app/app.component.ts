b1eed481e460169f9f9994f8d4292d624e573b17
Leonardo Pereira <leonardocoutope@gmail.com>
2023-07-17 08:46:32 -0300

    Usando o decorator @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jed';
+  luke = {name: "Luke", isJedi: true, temple: "Coruscant"}
+  leia = {name: "Leia", isJedi: false}
+  hanSolo = {name: "Han Solo", isJedi: false,}
 }
